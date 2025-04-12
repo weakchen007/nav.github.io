6a3ebd61d0b959fefceb9124202eadf73176dc41
AI智谷X <58799395+weakchen007@users.noreply.github.com>
2025-04-12 14:55:28 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// 网站配置和数据
+const siteData = {
+    sites: [
+        {
+            title: "Google",
+            imageUrl: "images/google.jpg",
+            link: "https://www.google.com"
+        },
+        {
+            title: "GitHub",
+            imageUrl: "images/github.jpg",
+            link: "https://github.com"
+        },
+        {
+            title: "Stack Overflow",
+            imageUrl: "images/stackoverflow.jpg",
+            link: "https://stackoverflow.com"
+        }
+        // 可以继续添加更多网站
+    ]
+};
